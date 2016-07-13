d8432ff3afa00958b9a7468dc988b9ccba3dc307
juicykiwi <joen0622@joen-MacBook-Pro.local>
2016-07-13 13:35:41 +0900

    NavMesh 테스트 샘플

---
+++ NavMesh/Assets/Dev/01_Script/Camera/MainCameraHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraHelper : MonoBehaviour {
+
+	public Transform _target = null;
+
+	Vector3 _beginPos = Vector3.zero;
+
+	void Awake () {
+		_beginPos = transform.position;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_target)
+		{
+			Vector3 pos = Vector3.zero;
+			pos.x = _target.position.x + _beginPos.x - 3.0f;
+			pos.z = _target.position.z + _beginPos.z - 5.0f;
+			pos.y = _beginPos.y;
+
+			transform.position = pos;
+		}
+	}
+}
---
+++ NavMesh/Assets/Dev/01_Script/Character/ChaAlly.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaAlly : MonoBehaviour {
+
+	public Vector3 targetPos = Vector3.zero;
+
+	private NavMeshAgent _agent = null;
+
+	void Awake () {
+		targetPos = gameObject.transform.position;
+
+		_agent = this.GetComponent<NavMeshAgent>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_agent.SetDestination(targetPos);
+	}
+
+	public void SetTarget(Transform target)
+	{
+		this.targetPos = target.position;
+	}
+}
